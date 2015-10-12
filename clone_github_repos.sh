162452cf2a1e7a91d7e2fb06ea27cd5920838849
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2015-10-12 08:02:22 -0700

    Tweaks to github cloning script

--- clone_github_repos.sh
+++ clone_github_repos.sh
@@ -5,7 +5,7 @@ YELLOW=$(tput setaf 190)
 BLUE=$(tput setaf 153)
 
 # Clone Github repos
-echo "\n${YELLOW}This script will clone the Github repos which you own.${NORMAL}"
+echo "\n${YELLOW}This script will clone the Github repos which you own and put them in ~/git${NORMAL}"
 echo "\n${BLUE}Enter your Github username:${NORMAL}"
 read username
 
@@ -13,6 +13,6 @@ echo "\n${BLUE}Generate a personal access token (https://github.com/settings/tok
 read token
 
 echo "\n${YELLOW}Cloning repos${NORMAL}"
+mkdir ~/git
 cd ~/git
-# curl -s https://api.github.com/users/${username}/repos?per_page=200 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
 curl -u ${username}:${token} https://api.github.com/user/repos?type=owner | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
